9f67ed690dd9e2a989b06070f3eb765ce895c491
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-02-04 14:16:23 -0300

    Add project files.

---
+++ RevitAddin.PanelColorExample/Revit/App.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using ricaun.Revit.UI;
+using System;
+
+namespace RevitAddin.PanelColorExample.Revit
+{
+    [Console]
+    public class App : IExternalApplication
+    {
+        static RibbonPanel ribbonPanel;
+        public Result OnStartup(UIControlledApplication application)
+        {
+            ribbonPanel = application.CreatePanel("Color");
+            ribbonPanel.AddPushButton<Commands.Command>()
+                .SetLargeImage(@"https://img.icons8.com/small/32/0000ff/menu.png".GetBitmapSource());
+
+            ribbonPanel.GetRibbonPanel().CustomPanelTitleBarBackground = System.Windows.Media.Brushes.LightBlue;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            ribbonPanel.Remove();
+            return Result.Succeeded;
+        }
+    }
+}
